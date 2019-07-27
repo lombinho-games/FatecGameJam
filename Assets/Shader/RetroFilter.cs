f99ea74adf8425e570b750159969f6f0d7104737
Camargod <38335652+Camargod@users.noreply.github.com>
2019-07-27 09:00:09 -0300

    Pixelization completed and mock is in development

---
+++ Assets/Shader/RetroFilter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RetroFilter : MonoBehaviour
+{
+    public Material effectMaterial;
+    [ExecuteInEditMode]
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void onRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination,effectMaterial);
+    }
+}
