5b9f85eb5a60f5e171b5d7169820c760c905859e
Jhonsme <jonasrpg00@gmail.com>
2019-07-27 18:14:50 -0300

    drag dropa
    Drag Drop cs

---
+++ Assets/Interagir.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interagir : MonoBehaviour
+{
+   public Move lupa;
+   void OnMouseOver()
+   {
+       if (lupa.lig)
+       {
+           if(Input.GetMouseButtonDown(0))
+           {
+            Debug.Log("TA LIGADO E FUNCIONANDO");
+           }
+       }
+   }
+}
---
+++ Assets/SpeechManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class SpeechManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public string[] texts;
+    public int currentText;
+    public Sprite currentCharacterImage;
+
+    public Image canvasImage;
+    public Text canvasText;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void RefreshGUI(){
+        currentText = 0;
+        canvasImage.sprite = currentCharacterImage;
+        canvasText.text = texts[currentText];
+    }
+}
