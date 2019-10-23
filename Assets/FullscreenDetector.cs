64edc67cb2bdb1f2cb744791514151c7e5f7d870
Victor Branco <victor.branco@dp6.local>
2019-10-23 17:46:40 -0300

    Menu Inicial Completo

---
+++ Assets/FullscreenDetector.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+public class FullscreenDetector : MonoBehaviour
+{
+    Dropdown dropdown;
+    // Start is called before the first frame update
+    void Start()
+    {
+        dropdown = GetComponent<Dropdown>();
+
+        int cont = 0;
+        foreach(FullScreenMode f in Enum.GetValues(typeof(FullScreenMode))){
+
+            Dropdown.OptionData data = new Dropdown.OptionData();
+            data.text = f.ToString();
+            dropdown.options.Add(data);
+
+            if(Screen.fullScreenMode == f){
+                dropdown.value = cont;
+            }
+            cont++;
+        }
+    }
+
+    public void ChangeFullscreen(){
+        FullScreenMode f = (FullScreenMode) Enum.GetValues(typeof(FullScreenMode)).GetValue(dropdown.value);
+        Screen.SetResolution(Screen.width, Screen.height, f);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/ResolutionDetector.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResolutionDetector : MonoBehaviour
+{
+
+    Dropdown dropdown;
+    // Start is called before the first frame update
+    void Start()
+    {
+        dropdown = GetComponent<Dropdown>();
+
+        int cont = 0;
+        foreach(Resolution res in Screen.resolutions){
+            Dropdown.OptionData data = new Dropdown.OptionData();
+            data.text = res.width + "x" + res.height;
+            dropdown.options.Add(data);
+
+            if(Screen.currentResolution.Equals(res)){
+                dropdown.value = cont;
+            }
+            cont++;
+        }
+    }
+
+    public void ChangeResolution(){
+        Resolution res = Screen.resolutions[dropdown.value];
+        Screen.SetResolution(res.width, res.height, Screen.fullScreenMode);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
